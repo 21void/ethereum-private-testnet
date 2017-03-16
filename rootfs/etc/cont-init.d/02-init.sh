dc7925c884d6fe1f2b6dc985bf12e2d32e28547d
SH <sh@analogic.cz>
2017-03-16 16:53:21 +0100

    prealloc

--- rootfs/etc/cont-init.d/02-init.sh
+++ rootfs/etc/cont-init.d/02-init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-geth --identity "node-1" --datadir "/data/node-1" --networkid 1900 init /etc/eth/customGenesis.json
-
-geth --identity "node-2" --datadir "/data/node-2" --networkid 1900 init /etc/eth/customGenesis.json
+# geth --identity "node-1" --datadir "/data/node-1" --networkid 1900 init /etc/eth/customGenesis.json
+# geth --identity "node-2" --datadir "/data/node-2" --networkid 1900 init /etc/eth/customGenesis.json
--- rootfs/etc/cont-init.d/03-accounts.sh
+++ rootfs/etc/cont-init.d/03-accounts.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-geth --datadir "/data/node-1" --password /dev/null account new shop
-
-geth --datadir "/data/node-2" --password /dev/null account new customer > /data/customer-account
+# geth --datadir "/data/node-1" --password /dev/null account new shop
+# geth --datadir "/data/node-2" --password /dev/null account new customer > /data/customer-account
