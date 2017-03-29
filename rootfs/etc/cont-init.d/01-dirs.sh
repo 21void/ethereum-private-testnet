c702f28d9eb43a4a605cfabe79b2045545ef8cb5
SH <sh@analogic.cz>
2017-03-29 11:43:50 +0200

    removing node 2

--- rootfs/etc/cont-init.d/01-dirs.sh
+++ rootfs/etc/cont-init.d/01-dirs.sh
@@ -8,6 +8,6 @@ if [ ! -d /data/node-1 ]; then
     mkdir /data/node-1
 fi
 
-if [ ! -d /data/node-2 ]; then
-    mkdir /data/node-2
-fi
+#if [ ! -d /data/node-2 ]; then
+#    mkdir /data/node-2
+#fi
